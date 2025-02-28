a07d8a72277cad2280f27f94a0218e6323217b17
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-02-28 03:33:20 +0300

    feat (models): added PhotoProtocol

---
+++ Gallery/Models/Photo/PhotoProtocol.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoProtocol.swift
+//  Gallery
+//
+//  Created by Andrej Hurynovič on 28.02.2025.
+//
+
+import Foundation
+
+protocol PhotoProtocol: Identifiable {
+    var id: String { get }
+    
+    var isFavorite: Bool { get }
+    
+    var width: Int { get }
+    var height: Int { get }
+    var hexadecimalColor: String { get }
+    
+    var publishedDate: Date { get }
+    var descriptionText: String? { get }
+    var alternativeDescriptionText: String? { get }
+    
+    var views: Int? { get }
+    var likes: Int { get }
+    var downloads: Int? { get }
+    
+    var location: Location? { get }
+    
+    var imageURL: String { get }
+    var downloadURL: String { get }
+}
