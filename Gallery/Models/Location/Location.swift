ad2c33cd79db69221f1116eb54db9b536707cda2
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-02-28 03:06:12 +0300

    feat (models): added Location model

---
+++ Gallery/Models/Location/Location.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  Gallery
+//
+//  Created by Andrej Hurynovič on 28.02.2025.
+//
+
+import Foundation
+
+struct Location {
+    let name: String
+    
+    let latitude: Decimal
+    let longitude: Decimal
+}
