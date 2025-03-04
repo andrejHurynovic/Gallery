7bef26c72fd5a0db018930514a5c0e9533bb4fee
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-03-05 01:56:31 +0300

    feat(services): added NetworkServiceError

---
+++ Gallery/Services/NetworkService/NetworkServiceError/NetworkServiceError.swift
@@ -0,0 +1,11 @@
+//
+//  NetworkServiceError.swift
+//  Gallery
+//
+//  Created by Andrej Hurynovič on 04.03.2025.
+//
+
+enum NetworkServiceError: Error {
+    case incorrectAPIKey
+    case rateLimitExceeded
+}
