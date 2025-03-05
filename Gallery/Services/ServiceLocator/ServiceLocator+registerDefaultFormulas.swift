096bb2bc91fd1605f1730733fc0315a4fac49ceb
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-03-05 03:02:10 +0300

    feat(services): added new formulas for ServiceLocator

--- Gallery/Services/ServiceLocator/ServiceLocator+registerDefaultFormulas.swift
+++ Gallery/Services/ServiceLocator/ServiceLocator+registerDefaultFormulas.swift
@@ -8,7 +8,9 @@
 extension ServiceLocator {
     static func registerDefaultFormulas() {
         ServiceLocator.shared.register(KeychainService() as KeychainServiceProtocol)
-        ServiceLocator.shared.register(ImageCacheService<ImageBox>() as ImageCacheService)
+        ServiceLocator.shared.register(ImageCacheService<ImageBox>() as ImageCacheServiceProtocol)
         ServiceLocator.shared.register(AlertService() as AlertServiceProtocol)
+        ServiceLocator.shared.register(DataService() as DataServiceProtocol)
+        ServiceLocator.shared.register(NetworkService() as NetworkServiceProtocol)
     }
 }
