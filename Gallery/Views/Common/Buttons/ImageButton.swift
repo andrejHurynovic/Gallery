9d746cd7f871ad0e613373d2ea115c0787f79de5
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-03-06 00:42:16 +0300

    feat(views): added ImageButton

---
+++ Gallery/Views/Common/Buttons/ImageButton.swift
@@ -0,0 +1,46 @@
+//
+//  ImageButton.swift
+//  Gallery
+//
+//  Created by Andrej Hurynovič on 05.03.2025.
+//
+
+import UIKit
+
+class ImageButton: UIButton {
+    var action: (() -> Void)?
+    
+    // MARK: - Initialization
+    
+    init(image: UIImage? = nil, action: (() -> Void)? = nil) {
+        self.action = action
+        super.init(frame: .zero)
+        setupButton(image: image)
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        adjustImageInsets()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    // MARK: - Private
+    
+    private func setupButton(image: UIImage?) {
+        self.setImage(image, for: .normal)
+        self.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    }
+    
+    private func adjustImageInsets() {
+        self.imageView?.contentMode = .scaleAspectFit
+        let inset = (bounds.height - Constants.UserInterface.largeIconSize) / 2
+        self.imageEdgeInsets = .init(top: inset, left: inset, bottom: inset, right: inset)
+    }
+    
+    @objc func buttonAction() {
+        action?()
+    }
+}
