c230b2ceb850c3123a8a0fa41b4fb0e291600318
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-03-07 20:28:15 +0300

    feat(services): updated data flow in DateService

--- Gallery/Services/DataService/DataService.swift
+++ Gallery/Services/DataService/DataService.swift
@@ -6,29 +6,39 @@
 //
 
 import Foundation
+import Combine
 
-final class DataService: DataServiceProtocol {
+final actor DataService: DataServiceProtocol {
     typealias ImageBoxType = ImageBox
     
     @Injected var networkService: (any NetworkServiceProtocol)?
     @Injected var imageCacheService: (any ImageCacheServiceProtocol)?
     
+    private var currentFetchPage = 1
     private var lastQuery: String?
     private var photosIDs: Set<String> = []
     
-    private var currentFetchPage = 0
+    private var temporaryFavoritePhotosProvided: Int = 0
+    private var temporaryFavoritePhotos: [any PhotoProtocol] = []
     
-    // MARK: - Public
+    var photosUpdatePublisher = PassthroughSubject<any PhotoProtocol, Never>()
+}
+
+// MARK: - Photos
+extension DataService {
     func getPhoto(for id: String) async -> (any PhotoProtocol)? {
         guard let data = await networkService?.fetch(APIEndpoint.photo(id: id)),
               let photo = try? JSONDecoder().decode(Photo.self, from: data) else { return nil }
         return photo
     }
-    
+}
+
+// MARK: - Photos
+extension DataService {
     func getPhotos(for query: String?) async -> [any PhotoProtocol]? {
         if lastQuery != query {
             lastQuery = query
-            currentFetchPage = 0
+            currentFetchPage = 1
             photosIDs.removeAll()
         }
         let photos: [Photo]?
@@ -38,7 +48,6 @@ final class DataService: DataServiceProtocol {
             photos = await fetchPhotos()
         }
         guard var photos else { return nil }
-        
         photos.removeAll { photosIDs.contains($0.id) }
         photosIDs.formUnion(photos.map { $0.id })
         
@@ -46,24 +55,26 @@ final class DataService: DataServiceProtocol {
         return photos
     }
     
-    func scaledImage(for requirements: any ImageRequirementsProtocol) async -> (any ImageBoxProtocol)? {
-        if let imageBox = imageCacheService?.getImage(requirements) { return imageBox }
-        
-        guard let data = await networkService?.fetch(APIEndpoint.imageWithRequirements(requirements)),
-              let imageBox = ImageBoxType(from: data) else { return nil }
-        imageCacheService?.addImage(id: requirements.id, imageBox)
-        return imageBox
+    func getFavoritePhotos() async -> [any PhotoProtocol]? {
+        guard temporaryFavoritePhotosProvided < temporaryFavoritePhotos.count else { return nil }
+        let bounds = temporaryFavoritePhotosProvided...(temporaryFavoritePhotosProvided+Constants.photosFetchPageSize)
+        let photos: [any PhotoProtocol].SubSequence
+        if bounds.upperBound > temporaryFavoritePhotos.count {
+            photos = (temporaryFavoritePhotos[temporaryFavoritePhotosProvided...])
+        } else {
+            photos = (temporaryFavoritePhotos[bounds])
+        }
+        temporaryFavoritePhotosProvided += photos.count
+        return Array(photos)
     }
     
-    func rawImage(for photo: Photo) async -> (any ImageBoxProtocol)? {
-        return nil
+    func changePersistenceStatus(for photo: any PhotoProtocol, isFavorite: Bool) async {
+        var photo = photo
+        photo.isFavorite = isFavorite
+        temporaryFavoritePhotos.append(photo)
+        photosUpdatePublisher.send(photo)
     }
     
-    func downloadImage(for photo: any PhotoProtocol) async -> (any ImageBoxProtocol)? {
-        return nil
-    }
-    
-    // MARK: - Private
     private func fetchPhotos() async -> [Photo]? {
         guard let data = await networkService?.fetch(APIEndpoint.photos(page: currentFetchPage, photosPerPage: Constants.photosFetchPageSize)) else { return nil }
         return try? JSONDecoder().decode([Photo].self, from: data)
@@ -73,5 +84,39 @@ final class DataService: DataServiceProtocol {
         guard let data = await networkService?.fetch(APIEndpoint.photosSearch(page: currentFetchPage, photosPerPage: Constants.photosFetchPageSize, query: query)),
               let result = try? JSONDecoder().decode(PhotosSearchResult.self, from: data) else { return nil }
         return result.results
+    }
+}
+
+// MARK: - Images
+extension DataService {
+    func scaledImage(for requirements: any ImageRequirementsProtocol) async -> (any ImageBoxProtocol)? {
+        if let imageBox = imageCacheService?.getImage(requirements) { return imageBox }
+        
+        guard let imageBox = await fetchImageBox(for: APIEndpoint.imageWithRequirements(requirements)) else { return nil }
+        imageCacheService?.addImage(id: requirements.id, imageBox)
+        return imageBox
+    }
+    
+    func rawImage(for photo: any PhotoProtocol) async -> (any ImageBoxProtocol)? {
+        let requirements = ImageRequirements(id: photo.id,
+                                             imageURL: photo.imageURL,
+                                             requiredWidth: CGFloat(photo.width),
+                                             requiredHeight: CGFloat(photo.height))
+        guard let imageBox = await fetchImageBox(for: APIEndpoint.imageWithRequirements(requirements)) else { return nil }
+        imageCacheService?.addImage(id: requirements.id, imageBox)
+        return imageBox
+    }
+    
+    func downloadImage(for photo: any PhotoProtocol) async -> (any ImageBoxProtocol)? {
+        guard let urlData = await networkService?.fetch(APIEndpoint.imageDownload(url: photo.downloadURL)),
+              let urlContainer = try? JSONDecoder().decode(PhotoDownloadURL.self, from: urlData),
+              let data = await networkService?.fetch(APIEndpoint.imageDownload(url: urlContainer.url)) else { return nil }
+        return ImageBox(from: data)
+    }
+    
+    private func fetchImageBox(for endpoint: any APIEndpointProtocol) async -> (any ImageBoxProtocol)? {
+        guard let data = await networkService?.fetch(endpoint),
+              let imageBox = ImageBoxType(from: data) else { return nil }
+        return imageBox
     }
 }
--- Gallery/Services/DataService/DataServiceProtocol.swift
+++ Gallery/Services/DataService/DataServiceProtocol.swift
@@ -5,11 +5,19 @@
 //  Created by Andrej Hurynovič on 03.03.2025.
 //
 
-protocol DataServiceProtocol {
+import Combine
+
+protocol DataServiceProtocol: Actor {
+    var photosUpdatePublisher: PassthroughSubject<any PhotoProtocol, Never> { get }
+    
     func getPhoto(for id: String) async -> (any PhotoProtocol)?
+    
     func getPhotos(for query: String?) async -> [any PhotoProtocol]?
+    func getFavoritePhotos() async -> [any PhotoProtocol]?
     
     func scaledImage(for requirements: ImageRequirementsProtocol) async -> (any ImageBoxProtocol)?
     func rawImage(for photo: any PhotoProtocol) async -> (any ImageBoxProtocol)?
     func downloadImage(for photo: any PhotoProtocol) async -> (any ImageBoxProtocol)?
+    
+    func changePersistenceStatus(for photo: any PhotoProtocol, isFavorite: Bool) async
 }
