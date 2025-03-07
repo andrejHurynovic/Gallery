c65fa1365418ec283cb8d186f78b61b490bc071e
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-03-08 02:00:35 +0300

    feat: added DataSource enumeration to PhotosListViewModel

---
+++ Gallery/ViewModels/PhotosListViewModel+DataSource.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosListViewModel+DataSource.swift
+//  Gallery
+//
+//  Created by Andrej Hurynovič on 07.03.2025.
+//
+
+extension PhotosListViewModel {
+    enum DataSource {
+        case all
+        case favorite
+    }
+}
