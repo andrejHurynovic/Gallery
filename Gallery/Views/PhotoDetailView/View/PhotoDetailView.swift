798378d71f895434bddc2c56b7dcecbca59e6b75
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-03-08 06:25:06 +0300

    refactor(view): refactored PhotoDetailView

--- Gallery/Views/PhotoDetailView/View/PhotoDetailView.swift
+++ Gallery/Views/PhotoDetailView/View/PhotoDetailView.swift
@@ -9,14 +9,38 @@ import UIKit
 
 extension PhotoDetailViewController {
     final class View: UIView {
-        private let scrollView = UIScrollView()
-        private let stackView = UIStackView()
+        private let scrollView: UIScrollView = {
+            let scrollView = UIScrollView()
+            return scrollView
+        }()
+        
+        private let stackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .vertical
+            stackView.spacing = Constants.UserInterface.verticalSpacing
+            stackView.layoutMargins = UIEdgeInsets(top: 0,
+                                                   left: Constants.UserInterface.horizontalSpacing,
+                                                   bottom: 0,
+                                                   right: Constants.UserInterface.horizontalSpacing)
+            stackView.isLayoutMarginsRelativeArrangement = true
+            return stackView
+        }()
         
         private let actionsStackView = TopStackView()
-        private let imageView = UIImageView()
+        private let imageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.layer.cornerRadius = Constants.UserInterface.cornerRadius
+            imageView.clipsToBounds = true
+            imageView.contentMode = .scaleAspectFit
+            return imageView
+        }()
+        
         private let infoStackView = InfoStackView()
-        private let titleLabel = UILabel(font: .preferredFont(forTextStyle: .subheadline))
-        private let descriptionLabel = UILabel(font: .preferredFont(forTextStyle: .subheadline).withWeight(.light))
+        private let titleLabel: UILabel = UILabel(font: .preferredFont(forTextStyle: .subheadline))
+        private let descriptionLabel: UILabel = {
+            let label = UILabel(font: .preferredFont(forTextStyle: .subheadline).withWeight(.light))
+            return label
+        }()
         
         private var imageViewHeightConstraint: NSLayoutConstraint?
         
@@ -24,39 +48,38 @@ extension PhotoDetailViewController {
         override init(frame: CGRect) {
             super.init(frame: frame)
             setup()
+            makeConstraints()
         }
         
         required init?(coder: NSCoder) {
             super.init(coder: coder)
             setup()
+            makeConstraints()
         }
         
         // MARK: - Setup
         private func setup() {
             backgroundColor = .systemBackground
             
-            setupScrollView()
-            setupStackView()
-            setupImageView()
-            addStackViewElements()
+            addSubview(scrollView)
+            scrollView.addSubview(stackView)
+            stackView.addArrangedSubviews(imageView, actionsStackView, infoStackView, titleLabel, descriptionLabel)
         }
         
-        private func setupScrollView() {
+        // MARK: - Layout
+        private func makeConstraints() {
             scrollView.translatesAutoresizingMaskIntoConstraints = false
-            addSubview(scrollView)
+            stackView.translatesAutoresizingMaskIntoConstraints = false
             
+            // ScrollView
             NSLayoutConstraint.activate([
-                scrollView.topAnchor.constraint(equalTo: topAnchor),
+                scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
                 scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
                 scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
                 scrollView.trailingAnchor.constraint(equalTo: trailingAnchor)
             ])
-        }
-        
-        private func setupStackView() {
-            stackView.translatesAutoresizingMaskIntoConstraints = false
-            scrollView.addSubview(stackView)
             
+            // StackView
             NSLayoutConstraint.activate([
                 stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
                 stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
@@ -64,31 +87,18 @@ extension PhotoDetailViewController {
                 stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
                 stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
             ])
-            
-            stackView.axis = .vertical
-            stackView.spacing = Constants.UserInterface.verticalSpacing
-            stackView.layoutMargins = UIEdgeInsets(top: Constants.UserInterface.verticalSpacing,
-                                                   left: Constants.UserInterface.horizontalSpacing,
-                                                   bottom: Constants.UserInterface.verticalSpacing,
-                                                   right: Constants.UserInterface.horizontalSpacing)
-            stackView.isLayoutMarginsRelativeArrangement = true
-        }
-        
-        private func setupImageView() {
-            imageView.layer.cornerRadius = Constants.UserInterface.cornerRadius
-            imageView.clipsToBounds = true
-            imageView.contentMode = .scaleAspectFit
-        }
-        
-        private func addStackViewElements() {
-            stackView.addArrangedSubview(imageView)
-            stackView.addArrangedSubview(actionsStackView)
-            stackView.addArrangedSubview(infoStackView)
-            stackView.addArrangedSubview(titleLabel)
-            stackView.addArrangedSubview(descriptionLabel)
         }
         
         // MARK: - Public
+        func update(with photo: any PhotoProtocol) {
+            updateImageView(with: photo)
+            
+            actionsStackView.update(creationDate: photo.publicationDate, isFavorite: photo.isFavorite)
+            infoStackView.update(views: photo.views, downloads: photo.downloads, likes: photo.likes)
+            titleLabel.text = photo.alternativeDescriptionText
+            descriptionLabel.text = photo.descriptionText
+        }
+        
         func updateImage(image: UIImage?) {
             guard let image = image else {
                 imageView.image = nil
@@ -98,15 +108,6 @@ extension PhotoDetailViewController {
                               options: .transitionCrossDissolve) {
                 self.imageView.image = image
             }
-        }
-        
-        func update(with photo: any PhotoProtocol) {
-            updateImageView(with: photo)
-            
-            actionsStackView.update(creationDate: photo.publicationDate, isFavorite: photo.isFavorite)
-            infoStackView.update(views: photo.views, downloads: photo.downloads, likes: photo.likes)
-            titleLabel.text = photo.alternativeDescriptionText
-            descriptionLabel.text = photo.descriptionText
         }
         
         func updateActions(favoriteButtonAction: @escaping () -> Void,
