6ac0caa4f13a4d90adbade11c899a3762fd2b893
Hurynovič Andrej <dpkdpk10@icloud.com>
2025-03-08 04:43:42 +0300

    feat(views): added ReusablePageViewController

---
+++ Gallery/Views/Containers/ResuablePagesViewController/ReusablePageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReusablePageViewController 2.swift
+//  Gallery
+//
+//  Created by Andrej Hurynovič on 07.03.2025.
+//
+
+import UIKit
+
+class ReusablePageViewController: UIViewController {
+    var index: Int!
+    
+    func update(index: Int) {
+        self.index = index
+    }
+}
